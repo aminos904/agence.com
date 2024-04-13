496c4763a915ad46e001b7c3c1f6f04591e861fc
aminos904 <138410420+aminos904@users.noreply.github.com>
2024-04-12 23:58:34 -0400

    Delete agence.com directory

--- agence.com/database.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as db
-
-def connexionDB():
-    return db.connect(
-        user='root', 
-        password='',
-        host= 'localhost', 
-        database ='agence'
-    )
--- agence.com/employes/init.py
+++
@@ -1,7 +0,0 @@
-from employe_dao import  EmployeDao
-from employe import Employe
-
-#(message, Employe)=EmployeDao.get_all()
-#print("Message : ", message)
-#employe=Employe()
-
